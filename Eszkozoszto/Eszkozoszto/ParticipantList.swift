fb418b220ac79945a2b8f7202154d0bb132f40e6
Gujgiczer Anna <gujgiczer.anna@gmail.com>
2015-12-09 10:15:18 +0100

    added TableViewController and NavigationController

---
+++ Eszkozoszto/Eszkozoszto/ParticipantList.swift
@@ -0,0 +1,17 @@
+//
+//  ParticipantList.swift
+//  Eszkozoszto
+//
+//  Created by Gujgiczer Anna on 08/12/15.
+//  Copyright © 2015 Gujgiczer Anna. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class ParticipantList: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ Eszkozoszto/Participant.swift
@@ -0,0 +1,17 @@
+//
+//  Participant.swift
+//  Eszkozoszto
+//
+//  Created by Gujgiczer Anna on 08/12/15.
+//  Copyright © 2015 Gujgiczer Anna. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Participant: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
